7354b3707f651aa926f7c68f167205970c2cbf40
Habeeb Obalola <obalolah@yahoo.com>
2024-10-09 11:48:47 -0500

    added quotes to cidr block for security group rules

--- devops-projects/docker_cicd_github_action/main.tf
+++ devops-projects/docker_cicd_github_action/main.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "allow_tls" {
 
   resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = 0.0.0.0/0
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -82,7 +82,7 @@ resource "aws_security_group" "allow_tls" {
 
   resource "aws_vpc_security_group_egress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = 0.0.0.0/0
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
@@ -90,7 +90,7 @@ resource "aws_security_group" "allow_tls" {
 
   resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = 0.0.0.0/0
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
@@ -98,7 +98,7 @@ resource "aws_security_group" "allow_tls" {
 
   resource "aws_vpc_security_group_egress_rule" "allow_tls_https" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = 0.0.0.0/0
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
